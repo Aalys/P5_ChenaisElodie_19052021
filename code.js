ec6231543951a07daebb1ae5083d9836e49e00dd
Elodie Chenais <chenais.elodie@gmail.com>
2021-05-20 20:28:55 +0200

    Ajout html et test requête js

--- code.js
+++ code.js
@@ -1,2 +1,6 @@
-
-document.getElementById().innerHTML = ;
+// get("http://localhost:3000/api/cameras").then
+// document.getElementById().innerHTML = 
+fetch("http://localhost:3000/api/cameras")
+  .then(function(res) {
+    console.log(res)
+  })
