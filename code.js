159a18c3071449e5442d0bbed21fa17bf1d38da4
Elodie Chenais <chenais.elodie@gmail.com>
2021-05-23 12:22:28 +0200

    fetch test'

--- code.js
+++ code.js
@@ -1,33 +1,35 @@
-const img = document.getElementById(img) 
+// const img = document.getElementById(img) 
 
-// ====== Récupérer les produits depuis l'API ===== 
 
-const articles = async function() {
-  try {
-    let response = await fetch("http://localhost:3000/api/cameras") 
-    if (response.ok) {
-      let data = await response.json() // await attendre la résolution de la promesse
-        console.log(data)
-    } else {
-      console.error('Retour du serveur :', response.status)
-    }
-  } catch (e) {  //Capture les erreurs globalement 
-    console.log(e)
-  }
-}
+// const articles = async function() {
+  //   try {
+    //     let response = await fetch("http://localhost:3000/api/cameras") 
+    //     if (response.ok) {
+      //       let data = await response.json() // await attendre la résolution de la promesse
+//         console.log(data)
+//     } else {
+  //       console.error('Retour du serveur :', response.status)
+  //     }
+  //   } catch (e) {  //Capture les erreurs globalement 
+  //     console.log(e)
+  //   }
+  // }
+  
 
-// fetch("http://localhost:3000/api/cameras")
-//     .then(function(res) {
-//       if (res.ok) {
-//         return res.json();
-//       }
-//     })
-//     .then(function(value) {
-//       console.log(value);
-//     })
-//     .catch(function(err) {
-//       // Une erreur est survenue
-//   });
+  // ====== Récupérer les produits depuis l'API ===== 
+  
+fetch("http://localhost:3000/api/cameras")
+    .then(function(res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function(value) {
+      console.log(value);
+    })
+    .catch(function(err) {
+      // Une erreur est survenue
+  });
 
 
 
